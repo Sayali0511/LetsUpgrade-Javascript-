c1cdcf004426f88027250dcc5f8c0dd16efdd4c5
Sayali0511 <68383645+Sayali0511@users.noreply.github.com>
2020-07-16 19:59:00 +0530

    Delete UserInput.js

--- UserInput.js
+++
@@ -1,5 +0,0 @@
-let name = "Sonali";
-let college_name = "ACFGH";
-let city = "Solapur";
-
-console.log("The name is " + name + " .His college name is " + college_name + " and his native place is " + city);
